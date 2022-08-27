4e261438247ab363d1b445ede2d6dbac1cd3a81a
eduardcoder <eduardsaxofon@gmail.com>
2022-08-27 23:27:21 +0200

    Creacion de projecto con sprint boot.

--- README.md
+++
@@ -1,2 +0,0 @@
-# misionTIC_c3
-Eduardo Andres Caballero Leon (Scrum Master & desarrollador) eduardsaxofon@gmail.com
---
+++ src/test/java/EdSoftwareServicios/appMisionTIC/AppMisionTicApplicationTests.java
@@ -0,0 +1,13 @@
+package EdSoftwareServicios.appMisionTIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMisionTicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
